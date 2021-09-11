70b6ad0e7e40d16dd28b0bbf1a9c8fa28d6cbd35
Craig Neff <ifly12610@gmail.com>
2021-09-10 23:45:56 -0600

    started on right side of works section

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -20,7 +20,12 @@ export default function Works() {
                 <span>Projects</span>
               </div>
             </div>
-            <div className="right"></div>
+            <div className="right">
+              <img
+                src="https://res.cloudinary.com/practicaldev/image/fetch/s--fBO0Zf3i--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/q97aza64off758w3wfdo.jpg"
+                alt=""
+              />
+            </div>
           </div>
         </div>
       </div>
