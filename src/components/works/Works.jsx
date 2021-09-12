cfad7b02c8f3e08a36ac1267ee96cb65d38d215d
Craig Neff <ifly12610@gmail.com>
2021-09-12 00:38:29 -0600

    fixed slider effect

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,36 +1,38 @@
 import { useState } from "react";
 import "./works.scss";
+
 export default function Works() {
   const [currentSlide, setCurrentSlide] = useState(0);
-
   const data = [
     {
       id: "1",
-      icon: "pic here",
-      title: "title here",
-      desc: "Words here that are more words hsdfjahsdfhisdf sdf",
-      img: "shorturl.at/xyV35",
+      icon: "./assets/mobile.png",
+      title: "Web Design",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      img: "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
     },
     {
       id: "2",
-      icon: "pic here",
-      title: "#2",
-      desc: "Words here that are more words hsdfjahsdfhisdf sdf",
-      img: "shorturl.at/xyV35",
+      icon: "./assets/globe.png",
+      title: "Mobile Application",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img: "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
     },
     {
       id: "3",
-      icon: "pic here",
-      title: "title here##3",
-      desc: "Words here that are more words hsdfjahsdfhisdf sdf",
-      img: "shorturl.at/xyV35",
+      icon: "./assets/writing.png",
+      title: "Branding",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img: "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
     },
   ];
+
   const handleClick = (way) => {
     way === "left"
       ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
       : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
   };
+
   return (
     <div className="works" id="works">
       <div
@@ -38,13 +40,12 @@ export default function Works() {
         style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
       >
         {data.map((d) => (
-          //return cont
           <div className="container">
             <div className="item">
               <div className="left">
                 <div className="leftContainer">
                   <div className="imgContainer">
-                    <img src="{d.icon}" alt="" />
+                    <img src={d.icon} alt="" />
                   </div>
                   <h2>{d.title}</h2>
                   <p>{d.desc}</p>
@@ -53,16 +54,14 @@ export default function Works() {
               </div>
               <div className="right">
                 <img
-                  src="https://res.cloudinary.com/practicaldev/image/fetch/s--fBO0Zf3i--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/q97aza64off758w3wfdo.jpg"
+                  src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930"
                   alt=""
                 />
               </div>
             </div>
           </div>
-          //end
         ))}
       </div>
-
       <img
         src="assets/arrow.png"
         className="arrow left"
