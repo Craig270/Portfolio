5aa030e760f9080d68d1091b8cde2374a1625d10
Craig Neff <ifly12610@gmail.com>
2021-09-13 21:45:38 -0600

    started work on another section not sure if I will keep this one

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,7 +3,14 @@ import "./testimonials.scss";
 export default function Testimonials() {
   return (
     <div className="testimonials" id="testimonials">
-      This is the testimonials
+      <h1>Testimonials</h1>
+      <div className="card">
+        <div className="top">
+          <img src=""
+        </div>
+        <div className="center"></div>
+        <div className="bottom"></div>
+      </div>
     </div>
   );
 }
